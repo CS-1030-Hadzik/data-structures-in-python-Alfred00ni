73d098a456c45d258c5fc8866bbca61331302d91
Alfredo Bustamante <alfredobustamante@.mail.weber.edu>
2022-04-06 18:34:29 -0600

    Finshed Problems

---
+++ array.py
@@ -0,0 +1,58 @@
+
+from ctypes import cast
+from operator import invert
+from traceback import print_tb
+
+# Problem one
+print ('Alfredo Bustamante')
+
+# Problem Two
+cars = ['Ford','Chrysler','Dodge','Ram','Jeep','Chevy','GMC']
+
+# Problem Three
+print(cars)
+
+# Problem Four
+print(cars[0:7])
+
+# Problem Five
+cars.append('Buick')
+
+# Problem Six
+print(cars)
+
+# Problem Seven
+print(cars[4:5])
+
+# Problem Eight
+cars.insert(3,'Toyata')
+
+# Problme Nine
+print(cars)
+
+# Problme Ten
+cars.pop(5)
+
+# Problem Eleven
+print(cars)
+
+# Problem Twelve
+cars.sort()
+
+# Problem 13
+print(cars)
+
+# Problem 14
+cars.sort(reverse = True)
+
+# Problem 15
+print(cars)
+
+# Problem 16
+my_array_length = cars
+
+# Problem 17
+array_string = 'The length of my array is '
+
+# Problem 18 
+print(array_string, my_array_length)
